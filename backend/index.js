471d0249a84c3cc51edae2704a4ba5320e40d6bd
Sudhir Kumar <sk899626@gmail.com>
2024-09-20 17:40:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin: 'https://job-portal-frontend-rose.vercel.app/',
+    origin: 'https://job-portal-frontend-rose.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
