c8bda32387686b688ab8be061578ed70952d1b85
Sudhir Kumar <sk899626@gmail.com>
2024-09-26 22:07:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,9 +25,7 @@ app.use(cors(corsOptions));
 
 const PORT=process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.json("Hello");
-});
+
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
