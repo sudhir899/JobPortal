fa601da62fc390e60a7684d10e6f879da3b9859a
Sudhir Kumar <sk899626@gmail.com>
2024-09-26 21:55:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin: 'http://localhost:5173',
+    origin: 'https://job-portal-kohl-gamma.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
